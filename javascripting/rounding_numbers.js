cf8b4e744c797b76e547e1f693bc42814eee9033
erikarubi25 <erikarubi.2579@gmail.com>
2023-03-27 10:59:51 -0600

    repasar mas los arrays y for me cuesta entenderlos y el rounding

--- javascripting/rounding_numbers.js
+++ javascripting/rounding_numbers.js
@@ -1,6 +1,8 @@
 var roundUp = 1.5;
-var rounded = output;
+var rounded = Math.round(roundUp);
 
-Math.round(0.5); // Revisar error
-console.log();
+//rounded (Math.round(roundUp)); //Mi error fue escribir el metodo aqui, cuando solo tenia q definirlo en la varibale
+console.log(rounded);
+
+
 
